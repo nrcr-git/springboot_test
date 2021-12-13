3100eb59814f50632fac6f7c096ac605209973ad
nrcr-git <66444265+nrcr-git@users.noreply.github.com>
2021-12-12 20:56:55 -0500

    Update IdentifibleMarks.java
    Removed unwanted imports

--- src/main/java/com/ally/unicorns/entity/IdentifibleMarks.java
+++ src/main/java/com/ally/unicorns/entity/IdentifibleMarks.java
@@ -1,20 +1,8 @@
 package com.ally.unicorns.entity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class IdentifibleMarks {
